de4922c34dbbd7052dfcbf600ecd1ad81c49f0f0
dkp1903 <dushyantpathak6789@gmail.com>
2019-05-06 11:11:29 +0530

    Tried to fool around with the ID. Might help/Might not.

--- app/src/main/java/app/dkp1903/helpinghands/MainActivity.kt
+++ app/src/main/java/app/dkp1903/helpinghands/MainActivity.kt
@@ -1,12 +1,32 @@
 package app.dkp1903.helpinghands
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
+    var admloginbutton: Button? = null
+    var internloginbutton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        admloginbutton = findViewById(R.id.AdminLogin)
+        admloginbutton?.setOnClickListener({
+            var clickIntent2 = Intent(this@MainActivity, AdminLogin::class.java)
+            startActivity(clickIntent2)
+        })
+
+        internloginbutton = findViewById(R.id.LoginIntern)
+        internloginbutton?.setOnClickListener({
+            var clickIntent3 = Intent(this@MainActivity, InternLogin::class.java)
+            startActivity(clickIntent3)
+        })
     }
+
 }
+
